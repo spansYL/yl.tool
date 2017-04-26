7fbad7a45983ab6dc84563f2a71b43ab0cb9ebf1
dali <542621868@qq.com>
2017-04-26 10:49:27 +0800

    ï¼ï 	modified:   index.html modified:   index.jsï¼

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 /**
- * 各种地址验证
+ * 自定义jquery工具
+ * @author yl
  */
-function yl_check(){
-
-    //网址验证
-    this.checkUrl = function(url){
-        var reg = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/gi;
+$.extend({
+    //验证网址
+    tool_checkUrl: function(url){
+        var reg = /(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/gi;
         return  reg.test(url);
-    };
+    },
     //验证手机号
-    this.checkPhone = function(phone){
+    tool_checkPhone: function(phone){
         var reg = /^1(3|4|5|7|8)\d{9}$/;
         return reg.test(phone);
-    }
-    //保留两位小数点
-    this.twoDec = function(input) {
-        input = input || 0;
-        return parseFloat(input).toFixed(2);
-    }
+    },
+    //保留n位小数，默认2位
+    tool_nDec: function(num,n){
+        num = num || 0;
+        n = n || 2;
+        return parseFloat(num).toFixed(n);
+    },
     //价格格式
-    this.price = function(input){
+    tool_price: function(input){
         if(typeof(input) == 'number'){
             input = input + '';
         }
@@ -29,20 +30,27 @@ function yl_check(){
         input = input.replace(".","$#$").replace(/\./g,"").replace("$#$",".");
         input = input.replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3'); //只能输入两个小数
         return input;
-    }
-    //仅限正整数
-    this.positive = function(input){
+    },
+    //正整数
+    tool_positive: function(input){
         input = input.replace(/[^\d]/g,""); //清除"数字"以外的字符
         return input;
-    }
+    },
     //统计字符数
-    this.countWord = function(str){
+    tool_countChar: function(str){
         var result = str.replace(/[^\x00-\xff]/g,"01").length;
         return result;
-    };
-
+    },
+    //截取前n个字符,默认4个
+    tool_cutWord : function(str,n) {
+        n = n | 4;
+        var chinessArr = str.match(/[^\x00-\xff]/g);
+        var chineseNum = chinessArr ? chinessArr.length : 0;
+        var result = str.substring(0, n - chineseNum);
+        return result;
+    },
     //格式化千分位
-    this.formatThousands = function(num,n) {
+    tool_formatThousands:function(num,n) {
         var num = parseFloat(num).toFixed(n);
         var _num = String(num);
         var _num = _num.replace(/^(\d+)((\.\d+)?)$/, function (s, s1, s2) {
@@ -50,5 +58,5 @@ function yl_check(){
         });
         return _num;
     }
-  return this;
-}
+});
+   
